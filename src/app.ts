1f63bfea26971d508dd1c59a4637e34431b4a471
Ayush <62958067+Ayush-v@users.noreply.github.com>
2023-05-17 14:43:25 +0530

    Update to default behaviour app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log("hello world");
+import express, { Application } from "express";
+import { Prisma } from "@prisma/client";
+import dotenv from "dotenv";
+import morgan from "morgan";
+const app: Application = express();
+
+dotenv.config();
+app.use(express.json());
+app.use(morgan("dev"));
+
+const PORT = process.env.PORT || "8080";
+
+app.use("*", (req, res) => {
+  res.status(404).send(`${req.originalUrl} not found on this *server*`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Running at ${PORT} 🚀`);
+});
