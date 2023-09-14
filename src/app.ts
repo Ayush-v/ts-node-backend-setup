be7021414edc97f77844c9456a8b65d25c0d7326
Ayush <62958067+Ayush-v@users.noreply.github.com>
2023-09-15 02:27:18 +0530

    updated deps | switched to pnpm | added express-rate-limit

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application } from "express";
+import { rateLimit } from "express-rate-limit";
 import dotenv from "dotenv";
 import morgan from "morgan";
 const app: Application = express();
@@ -7,7 +8,21 @@ dotenv.config();
 app.use(express.json());
 app.use(morgan("dev"));
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  // store: ... , // Use an external store for more precise rate limiting
+});
+
 const PORT = process.env.PORT || "8080";
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World!" });
+});
 
 app.use("*", (req, res) => {
   res.status(404).send(`${req.originalUrl} not found on this *server*`);
